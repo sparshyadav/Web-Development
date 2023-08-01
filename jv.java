59b0972088427095659ae564f3848fa857b03bb6
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-01 19:11:37 +0530

    Updates

--- jv.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class jv {
-    public static void main(String[] args) {
-        List<int> intList= new ArrayList<>();
-        intList.add(10);
-        intList.add(20);
-        System.out.println(intList);
-    }
-}
