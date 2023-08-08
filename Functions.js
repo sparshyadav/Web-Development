2669eb68f64fccfed7d6f9ba79ef0fa944cac20d
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-08 18:54:00 +0530

    Updated

---
+++ Functions.js
@@ -0,0 +1,26 @@
+// By default undifined is returned for all the functions.
+
+// function add(a, b){
+//     return a+b;
+// }
+// console.log(add(3,5));
+
+// function sub(a, b){
+//     return a-b;
+// }
+// console.log(sub(-10, -20));
+
+// var sub=function(a, b){
+//     return a*b;
+// }
+// console.log(sub(10, 5));
+
+// function add(a, b){
+//     console.log(a+b);
+// }
+// add(10, 5);
+
+
+
+
+
