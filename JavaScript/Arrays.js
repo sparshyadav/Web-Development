d0f491346f6166c306d98aeaa6c8d46655388747
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-14 23:39:19 +0530

    Arrays in JavaScript

---
+++ JavaScript/Arrays.js
@@ -0,0 +1,11 @@
+let numbers=[1,4,5,8];
+console.log(numbers);
+
+numbers.push(19);
+console.log(numbes);
+
+numbers.unshift(0);
+console.log(numbers);
+
+numbers.splice(4, 5, 5, 5, 5);
+console.log(numbers);
