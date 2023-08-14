1297f3e3d8c803c15b2e2069d0ec0c6dc73d407b
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-14 10:10:31 +0530

    Math Class in JavaScript

---
+++ JavaScript/Math_Class.js
@@ -0,0 +1,15 @@
+console.log(Math.random()); //Math.random() gives a random number between 0 and 1.
+console.log(Math.random());
+
+console.log(Math.PI); //Math.PI returns the value of Pi.
+
+console.log(Math.round(1.2)); //Math.round() rounds of a floating point number into a integer number without decimal point.
+console.log(Math.round(7.9));
+console.log(Math.round(22.5));
+
+console.log(Math.max(2,5,3,6)); //Math.max() returns the maximum number from all the numbers given to it.
+
+console.log(Math.min(2,5,3,6)); //Math.max() returns the minimum number from all the numbers given to it.
+
+console.log(Math.abs(5)); //Math.abs() returns the absolute value, i.e. only positive, it converts the negative value into positive value.
+console.log(Math.abs(-5));
