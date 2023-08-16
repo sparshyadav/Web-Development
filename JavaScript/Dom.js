9c9c60af4dcbd88073abacb1750335ed71071d06
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-16 23:12:20 +0530

    DOM

---
+++ JavaScript/Dom.js
@@ -0,0 +1,15 @@
+const byId=document.getElementById('firstid');
+console.log(byId);
+
+const byClass=document.getElementsByClassName('firstclass');
+console.log(byClass);
+
+const byclass=document.getElementsByClassName('class');
+console.log(byclass);
+
+const tags=document.getElementsByTagName('h1');
+console.log(tags);
+
+const query=document.querySelector('.firstclass');
+console.log(query);
+
