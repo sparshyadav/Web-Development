475929e39b36a215140a348fdf5efe95b69fd01f
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-17 18:52:05 +0530

    innerText in JavaScript

--- JavaScript/DOM2.js
+++ JavaScript/DOM2.js
@@ -1,3 +1,7 @@
 const myHello=document.querySelector('h1');
 console.log(myHello.innerHTML);
 myHello.innerHTML="<h1>Hello World!</h1>";
+
+const para=document.querySelector('p');
+console.log(para.innerHTML);
+para.innerText='All I do is win win win';
