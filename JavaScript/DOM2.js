64377186c6a4e163862195309effba43eaeb2d37
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-17 19:13:30 +0530

    Styles in JavaScript

--- JavaScript/DOM2.js
+++ JavaScript/DOM2.js
@@ -5,3 +5,8 @@ myHello.innerHTML="<h1>Hello World!</h1>";
 const para=document.querySelector('p');
 console.log(para.innerHTML);
 para.innerText='All I do is win win win';
+
+myHello.style.color='blue';
+myHello.style.fontSize='50px';
+
+para.style.backgroundColor='lightgreen';
