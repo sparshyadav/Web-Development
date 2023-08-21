a65731b323b4c9dd46c2706b57f103a894019484
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-21 23:13:00 +0530

    querySelector in DOM

--- DOM/DOM Basics1.js
+++ DOM/DOM Basics1.js
@@ -6,3 +6,12 @@ console.log(var2);
 
 let var3=document.getElementsByClassName('para');
 console.log(var3);
+
+let var4=document.querySelector('#header');
+console.log(var4);
+
+let var5=document.querySelector('h1');
+console.log(var5);
+
+let var6=document.querySelector('.para');
+console.log(var6);
