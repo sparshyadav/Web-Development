abd1b930e11e236380890c50b20bb430a3c5e2e0
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-21 23:08:02 +0530

    GetElement in DOM

---
+++ DOM/DOM Basics1.js
@@ -0,0 +1,8 @@
+let var1=document.getElementById('header');
+console.log(var1);
+
+let var2=document.getElementsByTagName('h1');
+console.log(var2);
+
+let var3=document.getElementsByClassName('para');
+console.log(var3);
