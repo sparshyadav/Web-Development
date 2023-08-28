64ac4b37c0ac5f23eb3de5ab04dd771ff51da9f9
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-28 06:34:57 +0530

    RemoveEventListener in DOM

--- JavaScript/DOM3.js
+++ JavaScript/DOM3.js
@@ -1,14 +1,20 @@
-document.addEventListener('click', function () {
-    console.log("I clicked on document");
-});
+// document.addEventListener('click', function () {
+//     console.log("I clicked on document");
+// });
 
 let content=document.querySelector('h1');
 content.addEventListener('click', function(){
     content.style.backgroundColor="red";
 });
 
-let h2Color=document.querySelectorAll('h2');
+let h2Color=document.querySelector('h2');
 h2Color.addEventListener('click', function(){
     h2Color.style.backgroundColor="green";
     h2Color.style.color="orange";
 })
+
+function eventFunction(){
+    console.log("I have clicked on the document");
+}
+p.addEventListener('click', eventFunction());
+p.removeEventListener('click', eventFunction);
