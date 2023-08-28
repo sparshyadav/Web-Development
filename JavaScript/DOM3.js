9e349f7b3378ce6c3015e5ff115c885e6901aa8a
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-08-28 06:52:24 +0530

    PreventDefault in DOM

--- JavaScript/DOM3.js
+++ JavaScript/DOM3.js
@@ -11,10 +11,16 @@ let h2Color=document.querySelector('h2');
 h2Color.addEventListener('click', function(){
     h2Color.style.backgroundColor="green";
     h2Color.style.color="orange";
-})
+});
 
 function eventFunction(){
     console.log("I have clicked on the document");
 }
-p.addEventListener('click', eventFunction());
+p.addEventListener('click', eventFunction);
 p.removeEventListener('click', eventFunction);
+
+let link=document.querySelector('a');
+link.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log(`The href is ${this.href}`);
+});
