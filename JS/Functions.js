dd43bae06a93a35c9e0c2156ff0d0b1e45b7f3d9
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-09-12 10:37:36 +0530

    Arrow Functions

--- JS/Functions.js
+++ JS/Functions.js
@@ -1,23 +1,30 @@
-function calCartPrice(...num){
-    return num;
-}
-console.log(calCartPrice(1, 2, 3, 4, 5, 6));
+// function calCartPrice(...num){
+//     return num;
+// }
+// console.log(calCartPrice(1, 2, 3, 4, 5, 6));
 
-const user={
-    username: "Sparsh",
-    price: 1999
-}
+// const user={
+//     username: "Sparsh",
+//     price: 1999
+// }
 
-function handleObject(anyObject) {
-    console.log(`The UserName is: ${anyObject.username} and the price of object is: ${anyObject.price}`);
-}
-handleObject(user);
-handleObject({
-    username: "Yadav",
-    price: 999
-});
+// function handleObject(anyObject) {
+//     console.log(`The UserName is: ${anyObject.username} and the price of object is: ${anyObject.price}`);
+// }
+// handleObject(user);
+// handleObject({
+//     username: "Yadav",
+//     price: 999
+// });
 
-const addOne=function(num){ //This method of declaring a function using a variable is known as function expression.
-    return ++num;
+// const addOne=function(num){ //This method of declaring a function using a variable is known as function expression.
+//     return ++num;
+// }
+// console.log(addOne(5));
+
+
+// +++++++++ Arrow Functions ++++++++++
+const addTwo=(num1, num2)=>{
+    return num1+num2;
 }
-console.log(addOne(5));
+console.log(addTwo(3,5));
