2d976bba62b679642fdc50c8394ec3cea611eea4
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-09-12 10:14:27 +0530

    Updated

--- JS/Functions.js
+++ JS/Functions.js
@@ -2,3 +2,17 @@ function calCartPrice(...num){
     return num;
 }
 console.log(calCartPrice(1, 2, 3, 4, 5, 6));
+
+const user={
+    username: "Sparsh",
+    price: 1999
+}
+
+function handleObject(anyObject) {
+    console.log(`The UserName is: ${anyObject.username} and the price of object is: ${anyObject.price}`);
+}
+handleObject(user);
+handleObject({
+    username: "Yadav",
+    price: 999
+});
