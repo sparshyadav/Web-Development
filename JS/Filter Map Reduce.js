efce35510ac07145e81df4ac5f7b59d095d53f85
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-09-13 12:46:19 +0530

    Filters in JavaScript

---
+++ JS/Filter Map Reduce.js
@@ -0,0 +1,54 @@
+// const myNum=[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newNums=myNum.filter((num)=> num>4);
+// console.log(newNums);
+
+// const questions = [
+//     {
+//         question: 'What does the abbreviation HTML stand for?',
+//         answers: [
+//             { text: 'Hyper Text Markup Language', correct: true },
+//             { text: 'Hyper Transfer Markup Language', correct: false },
+//             { text: 'High Text Markup Language', correct: false },
+//             { text: 'Hyper Text Markeup Language', correct: false },
+//         ]
+//     },
+//     {
+//         question: 'What does the abbreviation CSS stand for?',
+//         answers: [
+//             { text: 'Cascading Special Sheets', correct: false },
+//             { text: 'Cascading Style Sheets', correct: true },
+//             { text: 'Color and Style Sheets', correct: false },
+//             { text: 'None of the Above', correct: false }
+//         ]
+//     },
+//     {
+//         question: 'How many sizes of headers are available in HTML by default?',
+//         answers: [
+//             { text: '5', correct: false },
+//             { text: '7', correct: false },
+//             { text: '4', correct: false },
+//             { text: '6', correct: true }
+//         ]
+//     },
+//     {
+//         question: 'In how many ways can CSS be written?',
+//         answers: [
+//             { text: '1', correct: false },
+//             { text: '2', correct: false },
+//             { text: '3', correct: true },
+//             { text: '4', correct: false }
+//         ]
+//     },
+//     {
+//         question: 'Which of the following does not require a closing tag?',
+//         answers: [
+//             { text: '<br>', correct: false },
+//             { text: '<hr>', correct: false },
+//             { text: 'Both A and B', correct: true },
+//             { text: 'None of the Above', correct: false }
+//         ]
+//     },
+// ];
+
+// const userQuiz=questions.filter((bk)=> answers[bk]==='Both A and B');
+// console.log(userQuiz);
